78376050da24b045162214d6b8b7cd2be5bbca6f
MohinKhan <mohinpathan2004@gmail.com>
2026-02-25 11:21:09 +0530

    Add files via upload

---
+++ ch13-Array/3-Adding_element.js
@@ -0,0 +1,31 @@
+//push is use to add element et the end
+
+ let heros=["Ironmen","Spidermen","Thor"];
+
+ heros.push("Hulk");
+
+console.log("After push",heros)
+
+//Unshift is use to add element at the first
+
+let car=["BMW","Honda","Porse"];
+
+car.unshift("Audi");
+
+console.log("After Unshift",car);
+
+//pop is use to Remove element from the end
+
+let bike=["KTM","Suzuki","Honda"]
+
+bike.pop();
+
+console.log("After pop",bike);
+
+//shift is use to remove element from the begin
+
+let frutes=["Banana","cherry","apple"];
+
+frutes.shift();
+
+console.log("After shift",frutes)
