1587ecdb2cd13e3b8cd21b8b3616e0d94817f813
MohinKhan <mohinpathan2004@gmail.com>
2026-03-03 12:04:37 +0530

    Add files via upload

---
+++ ch13-Array/9-Array.js
@@ -0,0 +1,41 @@
+
+//Find
+
+const number =[10,20,30,40,50,60,70,80,90,10];
+
+let total=number.find((check)=> check > 20);
+
+console.log(total);
+
+//FindIndex
+
+let indexcount =number.findIndex((check)=> check >70);
+
+console.log(indexcount);
+
+//slice
+
+const veges=["Ginger","Brokoly","Domstick","Coriyander"];
+
+console.log(veges.splice(2,3));
+
+//adding new veges in array
+
+console.log(veges.splice(1,0,"Potato"));
+
+console.log(veges);
+
+//Some and Every method
+
+let number1= [2,4,7,8,5,14,52,68,87,95,10];
+
+console.log("Some method example",number1.some((x) => x > 10));
+
+//every method example
+
+console.log("Every method example",number1.every((x)=>x >5));
+
+//True condition Example
+
+console.log("True condition Example",number1.every((y)=> y >1));
+
