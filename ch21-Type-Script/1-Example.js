ed0e2eb9490e9e196e7ff754a747946dd30790e7
Mohin Pathan <mohinpathan2004@gmail.com>
2026-04-16 11:25:12 +0530

    add this code

---
+++ ch21-Type-Script/1-Example.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num1 = 10;
+let num2 = 20;
+function multiple(a, b) {
+    return a * b;
+}
+console.log(multiple(5, 6));
+//# sourceMappingURL=1-Example.js.map
---
+++ ch21-Type-Script/1-Example.ts
@@ -0,0 +1,8 @@
+let num1:number=10;
+let num2:number=20;
+
+function multiple(a:number,b:number){
+    return a*b;
+}
+
+console.log(multiple(5,6));
