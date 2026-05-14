4b93ce3cb03b3e9e2a50364f1890b2bb3e486489
MohinKhan <mohinpathan2004@gmail.com>
2026-05-14 19:53:29 +0530

    Fix merge conflict in symbol.js
    Removed merge conflict markers and cleaned up code.

--- ch03-Data-Type/5-symbol.js
+++ ch03-Data-Type/5-symbol.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // This is symbol Data Type part of Primitive 
 // Example No:-1
 
@@ -14,5 +14,4 @@ let a = Symbol(10);
 let b = Symbol(10);
 
 console.log(a == b);
->>>>>>> ebcb54a8f2fc9f65ec674abe7f05616885dec8ff
 console.log(typeof Symbol);
